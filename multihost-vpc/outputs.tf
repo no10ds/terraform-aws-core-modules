6036f2f21154a6ce2a97e59d7c3ca6c859f4a2a2
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2022-06-14 13:36:57 +0100

    Add multihost vpn and subnet modules

---
+++ multihost-vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  value       = aws_vpc.core.id
+  description = "The ID of the created VPC"
+}
+
+output "vpc_cidr_range" {
+  value       = aws_vpc.core.cidr_block
+  description = "The ID of the created VPC"
+}
+
+output "private_route_table_ids" {
+  value       = aws_route_table.core_private_route_table[*].id
+  description = "A list of private route table IDs"
+}
